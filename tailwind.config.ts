871e3798ff4ebc0c690d1200675b326faefb1916
lopo <lopo@zju.edu.cn>
2023-11-28 23:34:27 +0800

    chore: tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default <Config>{
+    content: [
+        './src/**/*.tsx'
+    ],
+    theme: {
+        fontFamily: {},
+        colors: {},
+    },
+    plugins: [],
+}
